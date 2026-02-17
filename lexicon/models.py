1f6201dca9e12f23d7394c7cfce305c761c90309
alikashani1375 <s.a.s.kashani33@gmail.com>
2026-02-17 19:36:08 +0330

    Language Model Implemented

--- lexicon/models.py
+++ lexicon/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    """
+    Stores language metadata. 
+    Crucial for handling LTR (German) vs RTL (Farsi) in  frontend.
+    """
+
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10,unique=True)
+    direction = models.CharField(
+        max_length=3,
+        choices=[('ltr','Left-to-Right'),('rtl','Right-to-Left')],
+        default='ltr'
+    )
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+
